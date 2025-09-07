b99fa2cbdbf35bf5cabd26164cb538b8464bd341
Gloxiniaaa <hnm142758@gamil.com>
2025-09-07 14:34:47 +0700

    add TestNetworkManager scene

---
+++ Assets/_Game/Scripts/Constant.cs
@@ -0,0 +1,11 @@
+public class Constant
+{
+    public const int DEFAULT_LAYER = 0;
+    public const int IGNORE_LAYER = 2;
+    public const int PLAYER_LAYER = 6;
+    // public const int INTERACTABLE_LAYER = 3;
+    // public const int NOT_RENDER_LAYER = 11;
+    // public const int OUTLINE_LAYER = 12;
+
+    public const string PLAYER_TAG = "Player";
+}
