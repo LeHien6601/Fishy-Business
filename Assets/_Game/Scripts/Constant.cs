0488ddfe7860c5c42b041ad74c673ddc5a9c5ba2
Gloxiniaaa <hnm142758@gamil.com>
2025-09-18 16:47:24 +0700

    add interactor.cs and seat.cs

--- Assets/_Game/Scripts/Constant.cs
+++ Assets/_Game/Scripts/Constant.cs
@@ -3,7 +3,7 @@ public class Constant
     public const int DEFAULT_LAYER = 0;
     public const int IGNORE_LAYER = 2;
     public const int PLAYER_LAYER = 6;
-    // public const int INTERACTABLE_LAYER = 3;
+    public const int INTERACTABLE_LAYER = 3;
     // public const int NOT_RENDER_LAYER = 11;
     // public const int OUTLINE_LAYER = 12;
 
