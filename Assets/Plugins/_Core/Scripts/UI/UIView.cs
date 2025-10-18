193de2a5a0a81477dfa512334fc5ed030b26e4c1
LeHien6601 <lehien6601@gmail.com>
2025-10-18 11:31:36 +0700

    SCRUM-22 improve UIView system (prevent show/hide when playing other animation)

--- Assets/Plugins/_Core/Scripts/UI/UIView.cs
+++ Assets/Plugins/_Core/Scripts/UI/UIView.cs
@@ -4,55 +4,82 @@ using UnityEngine;
 [RequireComponent(typeof(Canvas))]
 public class UIView : MonoBehaviour
 {
+    #region Properties
     [Header("Animations")]
     [SerializeField] private UIAnimation _showAnimation;
     [SerializeField] private UIAnimation _hideAnimation;
     private bool _isShowing = false;
+    private bool _hasDoneShowAnimation = false;
+    private bool _hasDoneHideAnimation = true;
     private Canvas _canvas;
+    #endregion
     void Awake()
     {
         if (_canvas == null) _canvas = GetComponent<Canvas>();
     }
+
+    #region Getters/Setters
+    public bool IsShowing => _isShowing;
     public void SetSortingOrder(int order)
     {
         if (_canvas == null) _canvas = GetComponent<Canvas>();
         _canvas.overrideSorting = true;
         _canvas.sortingOrder = order;
     }
-    public virtual void Show()
+    #endregion
+    
+    #region Behaviors
+    public virtual async void Show()
     {
         if (_isShowing) return;
+        while (!_hasDoneHideAnimation)
+        {
+            await Task.Yield();
+        }
         _isShowing = true;
+        _hasDoneShowAnimation = false;
         gameObject.SetActive(true);
-        if (_showAnimation) _showAnimation.PlayAnimation();
+        if (_showAnimation) await _showAnimation.PlayAnimation();
+        _hasDoneShowAnimation = true;
     }
     public virtual async void Hide()
     {
         if (!_isShowing) return;
-        _isShowing = false;
-        if (_hideAnimation)
+        while (!_hasDoneShowAnimation)
         {
-            _hideAnimation.PlayAnimation();
-            await Task.Delay((int)(1000 * _hideAnimation.GetOverallDuration()));
+            await Task.Yield();
         }
+        _isShowing = false;
+        _hasDoneHideAnimation = false;
+        if (_hideAnimation) await _hideAnimation.PlayAnimation();
         gameObject.SetActive(false);
+        _hasDoneHideAnimation = true;
     }
-    public virtual void ShowWithParams(object param)
+    public virtual async void ShowWithParams(object param)
     {
         if (_isShowing) return;
+        while (!_hasDoneHideAnimation)
+        {
+            await Task.Yield();
+        }
         _isShowing = true;
+        _hasDoneShowAnimation = false;
         gameObject.SetActive(true);
-        if (_showAnimation) _showAnimation.PlayAnimation();
+        if (_showAnimation) await _showAnimation.PlayAnimation();
+        _hasDoneShowAnimation = true;
     }
     public virtual async void HideWithParams(object param)
     {
         if (!_isShowing) return;
-        _isShowing = false;
-        if (_hideAnimation)
+        while (!_hasDoneShowAnimation)
         {
-            _hideAnimation.PlayAnimation();
-            await Task.Delay((int)(1000 * _hideAnimation.GetOverallDuration()));
+            await Task.Yield();
         }
+        _isShowing = false;
+        _hasDoneHideAnimation = false;
+        if (_hideAnimation) await _hideAnimation.PlayAnimation();
         gameObject.SetActive(false);
+        _hasDoneHideAnimation = true;
     }
+    #endregion
 }
