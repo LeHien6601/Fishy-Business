d44ce40586d2bd31a46bd24209777f2cd503e22a
Gloxiniaaa <hnm142758@gamil.com>
2025-10-27 14:28:22 +0700

    NetworkBoardManager.cs: visualize deck on client-side

--- Assets/_Game/Scripts/Gameplay/Cards/NetworkBoardManager.cs
+++ Assets/_Game/Scripts/Gameplay/Cards/NetworkBoardManager.cs
@@ -10,21 +10,41 @@ public class NetworkBoardManager : NetworkBehaviour
 {
     [SerializeField] private CardDatabaseSO _cardDatabase;
     [SerializeField] private Card _cardPrefab;
+    [SerializeField] private Transform _deckPlace;
+    [SerializeField] private float _deckStackSpace = 0.002f;
+    private readonly Stack<Card> _freshCards = new(); // represents the deck of cards to be dealt
     private readonly List<CardHolder> _cardHolders = new(); // local cache of all card holders on the board, 1 is yours, the others are dummies representing other players' hands
-    private readonly List<CardData> _masterDeck = new();
+    private readonly List<CardData> _masterDeck = new(); // only server has the full deck data
 
+
+    #region  Setup logic
     public void StartGameLogic(NetworkList<ulong> playerOrders)
     {
         if (!IsServer) return;
 
         // Perform server-only setup
         InitializeAndShuffleDeck();
+        StartGameClientRpc();
         DealCards(playerOrders);
     }
 
-    #region  Setup logic server only
+    [ClientRpc]
+    private void StartGameClientRpc()
+    {
+        // spawn board, facing towards local player
+
+        // spawn deck,
+        _freshCards.Clear();
+        for (int i = 0; i < _cardDatabase.Size(); i++)
+        {
+            _freshCards.Push(Instantiate(_cardPrefab, _deckPlace.position + _deckStackSpace * i * Vector3.up, _cardPrefab.transform.rotation, _deckPlace));
+        }
+    }
+
     private void DealCards(NetworkList<ulong> playerOrders)
     {
+        if (!IsServer) return;
+
         int currentDeckIndex = 0;
 
         foreach (ulong clientId in playerOrders)
@@ -63,7 +83,7 @@ public class NetworkBoardManager : NetworkBehaviour
         {
             foreach (CardHolder holder in _cardHolders)
             {
-                Card newCard = Instantiate(_cardPrefab, transform.position, Quaternion.identity);
+                Card newCard = _freshCards.Pop();
                 if (holder.IsMine)
                 {
                     newCard.SetData(_cardDatabase.GetCardInforSO(cardData), CardLocation.Deck);
@@ -77,7 +97,8 @@ public class NetworkBoardManager : NetworkBehaviour
 
     private void InitializeAndShuffleDeck()
     {
-        // Example: Populate the master deck with card data
+        if (!IsServer) return;
+
         _masterDeck.Clear();
         for (int i = 0; i < _cardDatabase.Size(); i++)
         {
