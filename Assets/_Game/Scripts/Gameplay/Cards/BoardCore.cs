482c248c9037cc0d80ecfc89b90f8832e3159771
hocdang01 <hocdang01@gmail.com>
2025-11-02 19:13:55 +0700

    fix bug synd card rotation

--- Assets/_Game/Scripts/Gameplay/Cards/BoardCore.cs
+++ Assets/_Game/Scripts/Gameplay/Cards/BoardCore.cs
@@ -193,7 +193,7 @@ public class BoardCore : MonoBehaviour
 
     public void DropCardOntoBoard(Card card, Action onComplete = null)
     {
-        card.transform.DOLocalRotate(new Vector3(90f, 0f, 0f), FromHandToBoardDuration).SetEase(Ease.OutCubic);
+        card.transform.DOLocalRotate(new Vector3(90f, 0f, 0f), FromHandToBoardDuration* 0.5f).SetEase(Ease.OutCubic);
         card.transform.DOMove(_fromHandToBoardPos.position, FromHandToBoardDuration).SetEase(Ease.OutCubic).OnComplete(() =>
         {
             onComplete?.Invoke();
