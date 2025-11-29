41100982f422cd250c157b9d5a35c05c78e56175
hocdang01 <hocdang01@gmail.com>
2025-11-29 12:40:07 +0700

    add VFX into UIEndGame

--- Assets/_Game/Scripts/UI/UIEndGame.cs
+++ Assets/_Game/Scripts/UI/UIEndGame.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using DG.Tweening;
@@ -12,6 +13,13 @@ public class UIEndGame : UIView
     [SerializeField] private CustomLayout _layout;
     [SerializeField] private List<UIPlayerEndGame> _uiPlayers = new();
     [SerializeField] private TextMeshProUGUI _loseTMP;
+
+    [Header("VFX")]
+    [SerializeField] private Transform _fireWorkTransform;
+    [SerializeField] private Transform _sparksTransform;
+    [SerializeField] private ParticleSystem _fireWork;
+    [SerializeField] private ParticleSystem _sparks;
+
     #endregion
 
     #region View behavior
@@ -19,9 +27,18 @@ public class UIEndGame : UIView
     {
         UpdateData(GameplayManager.Instance.GetWinnerInfos());
         base.Show();
-        await Task.Delay((int)(1000 * Constant.RESTART_INTERVAL));
+        SpawnVFX();
+        // await Task.Delay((int)(1000 * Constant.RESTART_INTERVAL));
+        await Task.Delay((int)(1000 * 500));
         Hide();
         UIManager.Instance.HideUI(EUIState.InGame);
+    }
+
+    public override async void Hide()
+    {
+        base.Hide();
+        _fireWorkTransform.DeleteChildren();
+        _sparksTransform.DeleteChildren();
     }
     #endregion
 
@@ -56,6 +73,43 @@ public class UIEndGame : UIView
         {
             SoundManager.Play2D(SoundType.Victory);
         }
+    }
+    #endregion
+
+    #region VFX
+
+    private void SpawnVFX()
+    {
+        SpawnSparks();
+        StartCoroutine(SpawnFireworks(10));
+    }
+
+    private IEnumerator SpawnFireworks(int count)
+    {
+        var waitToSpawn = new WaitForSecondsRealtime(0.5f);
+        for (int i = 0; i < count; i++)
+        {
+            Vector3 randomOffset = new Vector3(
+                UnityEngine.Random.Range(-800f, 800f),
+                UnityEngine.Random.Range(-350f, 350f),
+                0f
+            );
+
+            Vector3 spawnPos = _fireWork.transform.localPosition + randomOffset;
+            var firework = Instantiate(_fireWork, _fireWorkTransform);
+            firework.transform.localPosition = _fireWork.transform.localPosition + randomOffset;
+            firework.transform.localRotation = _fireWork.transform.localRotation;
+            firework.Play();
+            yield return waitToSpawn;
+        }
+    }
+
+    private void SpawnSparks()
+    {
+        var sparks = Instantiate(_sparks, _sparksTransform);
+        sparks.transform.localPosition = Vector3.zero;
+        sparks.transform.localRotation = _sparks.transform.localRotation;
+        sparks.Play();
     }
     #endregion
 }
