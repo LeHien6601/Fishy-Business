dbcb309318f5aea2a38705e72af295ba1a7e00e8
hocdang01 <hocdang01@gmail.com>
2026-01-31 11:36:49 +0700

    feat: push missing script test cover

--- Assets/_Game/Scripts/Gameplay/GameMode/DayNightController.cs
+++ Assets/_Game/Scripts/Gameplay/GameMode/DayNightController.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class DayNightController : MonoBehaviour
 {
+    [SerializeField] private GameObject _coverImg;
     public void Cover()
     {
         Debug.Log("U r being covered, u cant see the board");
+        _coverImg.SetActive(true);
     }
 
     public void Uncover()
     {
         Debug.Log("U r uncovered, u can see the board");
+        _coverImg.SetActive(false);
     }
 }
