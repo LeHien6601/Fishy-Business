50186d319743c8cff2fa4deca40525fa4d778a51
Gloxiniaaa <hnm142758@gamil.com>
2026-04-07 17:31:32 +0700

    OutfitSelector: update outfit for late-joiners

--- Assets/_Game/Scripts/Gameplay/Player/FSM/OutfitSelector.cs
+++ Assets/_Game/Scripts/Gameplay/Player/FSM/OutfitSelector.cs
@@ -5,39 +5,24 @@ public class OutfitSelector : NetworkBehaviour
 {
     [SerializeField] private Transform _container;
     private NetworkVariable<OutfitPair> _outfitPair = new(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
-    private int _totalOutfit = -1;
     private int _currentOutfitId = 0;
 
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
         _outfitPair.OnValueChanged += OnOutfitChanged;
-        _totalOutfit = _container.childCount;
+
+        OnOutfitChanged(default, _outfitPair.Value);
     }
 
     private void OnOutfitChanged(OutfitPair previousValue, OutfitPair newValue)
     {
-        // if (previousValue.OutfitId == newValue.OutfitId)
-        // {
-        //     Transform oldVariant = _container.GetChild(previousValue.OutfitId).GetChild(previousValue.VariantId);
-        //     oldVariant.gameObject.SetActive(false);
-        // }
-
         Transform outfit = _container.GetChild(newValue.OutfitId);
         outfit.DisableChildren();
         Transform newVariant = outfit.GetChild(newValue.VariantId);
         newVariant.gameObject.SetActive(true);
     }
 
-
-    // UI can hook up to this method
-    public void OwnerChangeOutfit(int outfitId, int variantId)
-    {
-        if (!IsOwner)
-            return;
-        _currentOutfitId = outfitId;
-        _outfitPair.Value = new(outfitId, variantId);
-    }
     public void OwnerChangeOutfit(int outfitId, bool next)
     {
         if (!IsOwner)
@@ -59,51 +44,6 @@ public class OutfitSelector : NetworkBehaviour
             _outfitPair.Value = new(_currentOutfitId, variantId);
         }
     }
-
-#if UNITY_EDITOR
-/*
-    void Update()
-    {
-        if (!IsOwner)
-            return;
-
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            _currentOutfitId = (_currentOutfitId + 1) % _totalOutfit;
-            _outfitPair.Value = new(_currentOutfitId, 0);
-
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            _currentOutfitId = (_currentOutfitId - 1) % _totalOutfit;
-            if (_currentOutfitId < 0)
-            {
-                _currentOutfitId = _totalOutfit - 1;
-            }
-            _outfitPair.Value = new(_currentOutfitId, 0);
-        }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            int variantCount = _container.GetChild(_currentOutfitId).childCount;
-            int variantId = (_outfitPair.Value.VariantId + 1) % variantCount;
-            _outfitPair.Value = new(_currentOutfitId, variantId);
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            int variantCount = _container.GetChild(_currentOutfitId).childCount;
-            int variantId = (_outfitPair.Value.VariantId - 1) % variantCount;
-            if (variantId < 0)
-            {
-                variantId = variantCount - 1;
-            }
-            _outfitPair.Value = new(_currentOutfitId, variantId);
-        }
-    }
-*/
-# endif
-
-
 }
 
 public struct OutfitPair : INetworkSerializable
